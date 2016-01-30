dc11a8772e43bf9adc3d4cd516e9e8613d10f041
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2016-01-30 16:06:07 +0100

    items items

--- app/src/main/java/com/kitowcy/incrediblephysicsritual/fragments/CoilGameFragment.java
+++ app/src/main/java/com/kitowcy/incrediblephysicsritual/fragments/CoilGameFragment.java
@@ -1,6 +1,7 @@
 package com.kitowcy.incrediblephysicsritual.fragments;
 
 import android.app.Fragment;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,8 @@ public class CoilGameFragment extends Fragment {
     @Bind(R.id.piorun)
     ImageView piorun;
 
+//    AnimationDrawable animationDrawable;
+
     public static CoilGameFragment newInstace() {
         CoilGameFragment myFragment = new CoilGameFragment();
         return myFragment;
@@ -29,6 +32,16 @@ public class CoilGameFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.coil_game_fragment, container, false);
 
+//        piorun.setBackgroundResource(R.drawable.piorun_animation);
+//        animationDrawable = (AnimationDrawable) piorun.getBackground();
+
         return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+//        animationDrawable.start();
+
     }
 }
