3fec18b713b5f2e265b1dbd4e731a9e5d20c8595
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2016-01-31 12:50:06 +0100

    gaining exp improvements

--- app/src/main/java/com/kitowcy/incrediblephysicsritual/utils/Config.java
+++ app/src/main/java/com/kitowcy/incrediblephysicsritual/utils/Config.java
@@ -11,5 +11,11 @@ public class Config {
     public static final int FRAGMENT_STORY = 3;
     public static final int FRAGMENT_MEET_THE_TEAM = 4;
 
+    public static final int LEVEL_0 = 5;
+    public static final int LEVEL_1 = 15;
+    public static final int LEVEL_2 = 25;
+    public static final int LEVEL_3 = 50;
+    public static final int LEVEL_4 = 100;
+
 
 }
